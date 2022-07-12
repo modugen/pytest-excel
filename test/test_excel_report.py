d0996c5e3df9de6616897dd2f02501fab1a1e1ab
Clara Scherer <clara@modugen.de>
2022-07-12 14:38:28 +0200

    Add skip_models decorator

--- test/test_excel_report.py
+++ test/test_excel_report.py
@@ -19,14 +19,15 @@ def pytest_generate_tests(metafunc):
     """
     argnames = metafunc.fixturenames[:-1]
 
-    xfailed_models = get_xfailed_models(metafunc)
     all_models = set(metafunc.cls.models)
     model_markers = {
         model: [pytest.mark.model(model)]
         for model in all_models
     }
-    for model in xfailed_models:
+    for model in get_marked_models(metafunc, "xfail_models"):
         model_markers[model].append(pytest.mark.xfail(strict=True))
+    for model in get_marked_models(metafunc, "skip_models"):
+        model_markers[model].append(pytest.mark.skip)
     argvalues = [
         pytest.param(
             *[model for _ in range(len(argnames))],
@@ -39,21 +40,25 @@ def pytest_generate_tests(metafunc):
     )
 
 
-def get_xfailed_models(metafunc) -> Set[str]:
+def get_marked_models(metafunc, marker) -> Set[str]:
     """
-    Collect the content of all pytest.mark.xfail_model decorators on metafunc.
+    Collect the content of all pytest.mark.[marker] decorators on metafunc.
+
+    E.g. if marker == "xfail_models", the corresponding decorator is pytest.mark.xfail_models
     Args:
         metafunc(Metafunc):
             Represents a test function and its decorators, class etc.
             see https://docs.pytest.org/en/4.6.x/reference.html#metafunc
+        marker(str):
+            The name of the Mark
 
-    Returns: set of all xfailed model names
+    Returns: set of all model names contained in the decorator pytest.mark.[marker]
 
     """
     marks = getattr(metafunc.function, "pytestmark", [])
-    xfail_marks = [list(mark.args) for mark in marks if mark.name == "xfail_models"]
-    xfailed_models = set(itertools.chain.from_iterable(xfail_marks))
-    return xfailed_models
+    selected_marks = [list(mark.args) for mark in marks if mark.name == marker]
+    selected_models = set(itertools.chain.from_iterable(selected_marks))
+    return selected_models
 
 
 class TestExcelReportA(object):
@@ -72,7 +77,7 @@ class TestExcelReportA(object):
         """
         assert True
 
-    @pytest.mark.skip(reason="Skip for No Reason")
+    @pytest.mark.skip_models("fzkhaus")
     def test_excel_report_03(self, ifc_file):
         """
         Scenario: test_excel_report_01
