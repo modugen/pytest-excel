d27f48db34663fcf9e5b4ad815622916867a739d
Clara Scherer <clara@modugen.de>
2022-07-13 10:17:38 +0200

    Set model name for tests

--- test/test_normal_test_class.py
+++ test/test_normal_test_class.py
@@ -3,6 +3,7 @@ import pytest
 
 class TestExcelReportB(object):
 
+    @pytest.mark.model("my-model")
     def test_excel_report_01(self):
         """
         Scenario: test_excel_report_01
@@ -10,6 +11,7 @@ class TestExcelReportB(object):
         assert True
 
     @pytest.mark.xfail(reason="passed Simply", strict=True)
+    @pytest.mark.model("my-model")
     def test_excel_report_02(self):
         """
         Scenario: test_excel_report_02
@@ -17,6 +19,7 @@ class TestExcelReportB(object):
         assert True
 
     @pytest.mark.skip(reason="Skip for No Reason")
+    @pytest.mark.model("my-model")
     def test_excel_report_03(self):
         """
         Scenario: test_excel_report_01
@@ -24,12 +27,14 @@ class TestExcelReportB(object):
         assert True
 
     @pytest.mark.xfail(reason="Failed Simply")
+    @pytest.mark.model("my-model")
     def test_excel_report_04(self):
         """
         Scenario: test_excel_report_05
         """
         assert False
 
+    @pytest.mark.model("my-model")
     def test_excel_report_05(self):
         """
         Scenario: test_excel_report_06
